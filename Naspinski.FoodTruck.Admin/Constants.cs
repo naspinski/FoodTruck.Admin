fc92461f3c6dbb5d4005827e9265fe6d9cfc842c
naspinski <stan@naspinski.net>
2020-03-29 11:21:34 -0500

    adde din siblings

--- Naspinski.FoodTruck.Admin/Constants.cs
+++ Naspinski.FoodTruck.Admin/Constants.cs
@@ -53,8 +53,7 @@ namespace Naspinski.FoodTruck.AdminWeb
                         {
                             new Link() { Text = "Site Settings", CssClass = "btn btn-primary btn-lg btn-block", Controller = "Settings", Action = "Index", Glyphicon = "cog"},
                             new Link() { Text = "Profile", CssClass = "btn btn-default btn-lg btn-block", Controller = "Manage", Action = "Index", Glyphicon = "user"},
-                            //new Link() { Text = "Users", CssClass = "btn btn-primary btn-lg btn-block", Controller = "Users", Action = "Index", Glyphicon = "list"},
-                            //new Link() { Text = "Create User", CssClass = "btn btn-default btn-lg btn-block", Controller = "Account", Action = "Register", Glyphicon = "plus-sign"}
+                            new Link() { Text = "Sibling Sites", CssClass = "btn btn-primary btn-lg btn-block", Controller = "SiblingSite", Action = "Index", Glyphicon = "list"},
                             new Link() { Text = "Create User", CssClass = "btn btn-primary btn-lg btn-block", Controller = "Account", Action = "Register", Glyphicon = "plus-sign"},
                             new Link() { Text = "Images", CssClass = "btn btn-default btn-lg btn-block", Controller = "Images", Action = "Index", Glyphicon = "picture"},
                             new Link() { Text = "Documents", CssClass = "btn btn-primary btn-lg btn-block", Controller = "Documents", Action = "Index", Glyphicon = "paperclip"},
---
+++ Naspinski.FoodTruck.Admin/Controllers/SiblingSiteController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Naspinski.Data.Interfaces;
+using Naspinski.FoodTruck.Data;
+using Naspinski.FoodTruck.Data.Distribution.Handlers.Menu;
+using Naspinski.FoodTruck.Data.Distribution.Models.System;
+
+namespace Naspinski.FoodTruck.AdminWeb.Controllers
+{
+    [Authorize]
+    public class SiblingSiteController : _BaseFoodTruckController
+    {
+        private ICrudHandler<SiblingSiteModel, FoodTruckContext, SiblingSiteModel> _handler;
+
+        public SiblingSiteController(FoodTruckContext context) : base(context) { }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+            _handler = new SiblingSiteHandler(_context, _user);
+        }
+
+        [HttpGet]
+        public IActionResult Index(bool deleted = false)
+        {
+            ViewData["Deleted"] = deleted;
+            return View(_handler.GetAll(deleted));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id = 0)
+        {
+            return View(_handler.Get(id));
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id, bool isRestore = false)
+        {
+            if (isRestore) _handler.Restore(id);
+            else _handler.Delete(id);
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(SiblingSiteModel model)
+        {
+            return Json(_handler.Upsert(model));
+        }
+    }
+}
