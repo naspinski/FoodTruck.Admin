e638fc6a8d171ae9a25e9fa74d6b065d1828c0a0
naspinski <stan@naspinski.net>
2020-03-29 12:35:52 -0500

    styling and nav changes

--- Naspinski.FoodTruck.Admin/Constants.cs
+++ Naspinski.FoodTruck.Admin/Constants.cs
@@ -19,11 +19,9 @@ namespace Naspinski.FoodTruck.AdminWeb
                         Links = new List<Link>()
                         {
                             new Link() { Text = "Menu Items", CssClass = "btn btn-primary btn-lg btn-block", Controller = "Menu", Action = "Index", Glyphicon = "list"},
-                            new Link() { Text = "Create Menu Item", CssClass = "btn btn-default btn-lg btn-block", Controller = "Menu", Action = "Edit", Glyphicon = "plus-sign"},
-                            new Link() { Text = "Categories", CssClass = "btn btn-primary btn-lg btn-block", Controller = "Category", Action = "Index", Glyphicon = "list"},
-                            new Link() { Text = "Create Category", CssClass = "btn btn-default btn-lg btn-block", Controller = "Category", Action = "Edit", Glyphicon = "plus-sign"},
+                            new Link() { Text = "Categories", CssClass = "btn btn-default btn-lg btn-block", Controller = "Category", Action = "Index", Glyphicon = "list"},
                             new Link() { Text = "Price Types", CssClass = "btn btn-primary btn-lg btn-block", Controller = "PriceType", Action = "Index", Glyphicon = "list"},
-                            new Link() { Text = "Create Type", CssClass = "btn btn-default btn-lg btn-block", Controller = "PriceType", Action = "Edit", Glyphicon = "plus-sign"}
+                            new Link() { Text = "Orders", CssClass = "btn btn-default btn-lg btn-block", Controller = "Order", Action = "Index", Glyphicon = "shopping-cart"}
                         }
                     },
                     new LinkGroup()
@@ -35,12 +33,8 @@ namespace Naspinski.FoodTruck.AdminWeb
                         Links = new List<Link>()
                         {
                             new Link() { Text = "Specials", CssClass = "btn btn-primary btn-lg btn-block", Controller = "Special", Action = "Index", Glyphicon = "list"},
-                            new Link() { Text = "Create Special", CssClass = "btn btn-default btn-lg btn-block", Controller = "Special", Action = "Edit", Glyphicon = "plus-sign"},
-                            new Link() { Text = "Events", CssClass = "btn btn-primary btn-lg btn-block", Controller = "Event", Action = "Index", Glyphicon = "list"},
-                            new Link() { Text = "Create Event", CssClass = "btn btn-default btn-lg btn-block", Controller = "Event", Action = "Edit", Glyphicon = "plus-sign"},
+                            new Link() { Text = "Events", CssClass = "btn btn-default btn-lg btn-block", Controller = "Event", Action = "Index", Glyphicon = "list"},
                             new Link() { Text = "Locations", CssClass = "btn btn-primary btn-lg btn-block", Controller = "Location", Action = "Index", Glyphicon = "list"},
-                            new Link() { Text = "Create Location", CssClass = "btn btn-default btn-lg btn-block", Controller = "Location", Action = "Edit", Glyphicon = "plus-sign"},
-                            new Link() { Text = "Orders", CssClass = "btn btn-primary btn-lg btn-block", Controller = "Order", Action = "Index", Glyphicon = "shopping-cart"}
                         }
                     },
                     new LinkGroup()
@@ -53,11 +47,11 @@ namespace Naspinski.FoodTruck.AdminWeb
                         {
                             new Link() { Text = "Site Settings", CssClass = "btn btn-primary btn-lg btn-block", Controller = "Settings", Action = "Index", Glyphicon = "cog"},
                             new Link() { Text = "Profile", CssClass = "btn btn-default btn-lg btn-block", Controller = "Manage", Action = "Index", Glyphicon = "user"},
-                            new Link() { Text = "Sibling Sites", CssClass = "btn btn-primary btn-lg btn-block", Controller = "SiblingSite", Action = "Index", Glyphicon = "list"},
                             new Link() { Text = "Create User", CssClass = "btn btn-primary btn-lg btn-block", Controller = "Account", Action = "Register", Glyphicon = "plus-sign"},
                             new Link() { Text = "Images", CssClass = "btn btn-default btn-lg btn-block", Controller = "Images", Action = "Index", Glyphicon = "picture"},
                             new Link() { Text = "Documents", CssClass = "btn btn-primary btn-lg btn-block", Controller = "Documents", Action = "Index", Glyphicon = "paperclip"},
-                            new Link() { Text = "Delivery Limits", CssClass = "btn btn-default btn-lg btn-block", Controller = "DeliveryDestinations", Action = "Index", Glyphicon = "send"}
+                            new Link() { Text = "Delivery Limits", CssClass = "btn btn-default btn-lg btn-block", Controller = "DeliveryDestinations", Action = "Index", Glyphicon = "send"},
+                            new Link() { Text = "Sibling Sites", CssClass = "btn btn-primary btn-lg btn-block", Controller = "SiblingSite", Action = "Index", Glyphicon = "list"},
                         }
                     },
                     new LinkGroup()
