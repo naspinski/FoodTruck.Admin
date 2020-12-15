023647cd2af348ea14674ad7749beb9e0fd6779b
Stan Naspinski <stan@naspinski.net>
2020-12-15 16:03:17 -0600

    fixed Fri/Thurs and links

---
+++ Naspinski.FoodTruck.Admin/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Naspinski.FoodTruck.Admin.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string CamelCaseToSpacedString(this string s)
+        {
+            return Regex.Replace(s, "(\\B[A-Z])", " $1");
+        }
+    }
+}
---
+++ Naspinski.FoodTruck.Admin/Models/EditHeaderModel.cs
@@ -0,0 +1,13 @@
+﻿using Naspinski.FoodTruck.Admin.Extensions;
+
+namespace Naspinski.FoodTruck.Admin.Models
+{
+    public class EditHeaderModel
+    {
+        public bool IsNew;
+        public string Controller;
+        public string Text => Controller.CamelCaseToSpacedString();
+
+        public EditHeaderModel(bool isNew, string controller) => (IsNew, Controller) = (isNew, controller);
+    }
+}
