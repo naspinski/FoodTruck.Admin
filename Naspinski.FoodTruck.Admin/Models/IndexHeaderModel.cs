a3f0d6826f5261174b85075cfddb3b5e7b42d088
Stan Naspinski <stan@naspinski.net>
2020-12-15 16:43:42 -0600

    thursdays and factored out headers

---
+++ Naspinski.FoodTruck.Admin/Models/IndexHeaderModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Naspinski.FoodTruck.Admin.Models
+{
+    public class IndexHeaderModel
+    {
+        public string Controller;
+        public string Text;
+
+        public IndexHeaderModel(string controller, string text) => (Controller, Text) = (controller, text);
+    }
+}
